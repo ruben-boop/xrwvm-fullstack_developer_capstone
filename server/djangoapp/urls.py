469f50506e4ffc99a0f106e3399115d82dc9fc97
ruben-boop <nebur23sm@hotmail.com>
2026-06-05 17:03:24 -0400

    a

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,9 +7,9 @@ from . import views
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
-
+    path(route='registration', view=views.registration, name='registration'),
     # path for login
-     path(route='login', view=views.login_user, name='login'),
+    path(route='login', view=views.login_user, name='login'),
 
     # path for dealer reviews view
 
