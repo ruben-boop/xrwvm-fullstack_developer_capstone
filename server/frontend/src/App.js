a7f6a3e52af00e8f111aec4b68aba96dc6fc9be6
ruben-boop <nebur23sm@hotmail.com>
2026-06-05 17:03:02 -0400

    new message

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,5 +1,6 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
+import Register from "./components/Register/Register";
 
 function App() {
   return (
