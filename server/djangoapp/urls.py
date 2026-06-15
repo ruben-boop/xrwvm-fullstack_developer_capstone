e9884aa5b165e59231646c4d9d97f3f0ab653a76
ruben-boop <nebur23sm@hotmail.com>
2026-06-15 18:02:39 -0400

    aaaa

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
 
     # path for dealer reviews view
     path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
-        path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
 
     # path for add a review view
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
