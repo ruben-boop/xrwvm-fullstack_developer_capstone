7e6b91a1f079a3eec11027f4fcfd5d151fa6c049
ruben-boop <133923502+ruben-boop@users.noreply.github.com>
2026-06-19 16:48:46 -0600

    Fix import statements for Dealers and Dealer components

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import LoginPanel from "./components/Login/Login";
 import Register from "./components/Register/Register"; 
-import Dealers from "./components/Dealers/Dealers";
-import Dealer from "./components/Dealers/Dealer";
+import Dealers from './components/Dealers/Dealers';
+import Dealer from './components/Dealers/Dealer';
 import PostReview from "./components/Dealers/PostReview";
 import { Routes, Route } from "react-router-dom";
 
