41c7fb859c272c7ae4f48226966988c8386ced06
ruben-boop <133923502+ruben-boop@users.noreply.github.com>
2026-06-19 15:18:33 -0600

    Remove unused import for logout from views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -4,7 +4,6 @@
 # from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 # from django.shortcuts import get_object_or_404, render, redirect
-from django.contrib.auth import logout
 # from django.contrib import messages
 # from datetime import datetime
 
