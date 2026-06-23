f05f784d7a618f5d07579cd82f95bd7def70f467
ruben-boop <133923502+ruben-boop@users.noreply.github.com>
2026-06-23 17:39:32 -0600

    Fix formatting of request_url in post_review function

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -48,7 +48,7 @@ def analyze_review_sentiments(text):
 # def post_review(data_dict):
 # Add code for posting review
 def post_review(data_dict):
-    request_url = backend_url+"/insert_review"
+    request_url = backend_url+ "/insert_review"
     try:
         response = requests.post(request_url, json=data_dict)
         print(response.json())
