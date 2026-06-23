ed80edc8b724921b6ebea8327af0e08a23e71f19
ruben-boop <133923502+ruben-boop@users.noreply.github.com>
2026-06-23 17:48:21 -0600

    Refactor Dealers component structure and markup

--- server/frontend/src/components/Dealers/Dealers.jsx
+++ server/frontend/src/components/Dealers/Dealers.jsx
@@ -6,12 +6,9 @@ import review_icon from "../assets/reviewicon.png"
 
 const Dealers = () => {
   const [dealersList, setDealersList] = useState([]);
-  // let [state, setState] = useState("")
   let [states, setStates] = useState([])
 
-  // let root_url = window.location.origin
   let dealer_url ="/djangoapp/get_dealers";
-  
   let dealer_url_by_state = "/djangoapp/get_dealers/";
  
   const filterDealers = async (state) => {
@@ -53,42 +50,45 @@ return(
       <Header/>
 
      <table className='table'>
-      <tr>
-      <th>ID</th>
-      <th>Dealer Name</th>
-      <th>City</th>
-      <th>Address</th>
-      <th>Zip</th>
-      <th>
-      <select name="state" id="state" onChange={(e) => filterDealers(e.target.value)}>
-      <option value="" selected disabled hidden>State</option>
-      <option value="All">All States</option>
-      {states.map(state => (
-          <option value={state}>{state}</option>
-      ))}
-      </select>        
-
-      </th>
-      {isLoggedIn ? (
-          <th>Review Dealer</th>
-         ):<></>
-      }
-      </tr>
-     {dealersList.map(dealer => (
-        <tr>
-          <td>{dealer['id']}</td>
-          <td><a href={'/dealer/'+dealer['id']}>{dealer['full_name']}</a></td>
-          <td>{dealer['city']}</td>
-          <td>{dealer['address']}</td>
-          <td>{dealer['zip']}</td>
-          <td>{dealer['state']}</td>
-          {isLoggedIn ? (
-            <td><a href={`/postreview/${dealer['id']}`}><img src={review_icon} className="review_icon" alt="Post Review"/></a></td>
-           ):<></>
-          }
-        </tr>
-      ))}
-     </table>;
+       <thead>
+         <tr>
+           <th>ID</th>
+           <th>Dealer Name</th>
+           <th>City</th>
+           <th>Address</th>
+           <th>Zip</th>
+           <th>
+             <select name="state" id="state" onChange={(e) => filterDealers(e.target.value)}>
+               <option value="" selected disabled hidden>State</option>
+               <option value="All">All States</option>
+               {states.map(state => (
+                   <option value={state}>{state}</option>
+               ))}
+             </select>        
+           </th>
+           {isLoggedIn ? (
+               <th>Review Dealer</th>
+              ):<></>
+           }
+         </tr>
+       </thead>
+       <tbody>
+         {dealersList.map(dealer => (
+           <tr key={dealer.id}>
+             <td>{dealer['id']}</td>
+             <td><a href={'/dealer/'+dealer['id']}>{dealer['full_name']}</a></td>
+             <td>{dealer['city']}</td>
+             <td>{dealer['address']}</td>
+             <td>{dealer['zip']}</td>
+             <td>{dealer['state']}</td>
+             {isLoggedIn ? (
+               <td><a href={`/postreview/${dealer['id']}`}><img src={review_icon} className="review_icon" alt="Post Review"/></a></td>
+              ):<></>
+             }
+           </tr>
+         ))}
+       </tbody>
+     </table>
   </div>
 )
 }
